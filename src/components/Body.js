ebef75200df25914a658a4a87ead4a6c96ad9dbe
Shreya-1206 <shreyaray.1206@gmail.com>
2024-06-20 11:06:43 +0530

    updated resList Again

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,56 +3,7 @@ import { useState } from "react";
 import resList from "../utils/mockData";
 const Body =() => {    // not gonan use mock data reslist
     //State Variable
-   let [listOfRes, setListOfRes] = useState([
-    {
-        "info": {
-            "id": "651010",
-            "name": "Pizza Hut",
-            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-            "costForTwo": "₹350 for two",
-            "cuisines": [
-                "Pizzas"
-            ],
-            "avgRating": 3.8,
-            "sla": {
-                "deliveryTime": 30
-            },
-            
-        },        
-    },
-    {
-        "info": {
-            "id": "651011",
-            "name": "MCD",
-            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-            "costForTwo": "₹350 for two",
-            "cuisines": [
-                "Burgers"
-            ],
-            "avgRating": 4.3,
-            "sla": {
-                "deliveryTime": 30
-            },
-            
-        },        
-    },
-    {
-        "info": {
-            "id": "651012",
-            "name": "KFC",
-            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-            "costForTwo": "₹350 for two",
-            "cuisines": [
-                "Pizzas"
-            ],
-            "avgRating": 4.0,
-            "sla": {
-                "deliveryTime": 30
-            },
-            
-        },        
-    },
-   ])
+   let [listOfRes, setListOfRes] = useState(resList);
 
     //Normal Js Variable 
 //     let listOfRes = [
@@ -111,7 +62,7 @@ const Body =() => {    // not gonan use mock data reslist
             () => {
                 // console.log("Button Clicked ❣️")
                let filterRes = listOfRes.filter((res) => res.info.avgRating >= 4);
-               setListOfRes(filterRes)
+               setListOfRes(filterRes);
                 console.log(filterRes);
             }
         }>Top Rated Restaurants</button>
